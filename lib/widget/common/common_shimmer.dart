923a71cd16bc721d12806c7681067cf7b54bb039
小滋润 <807046079@qq.com>
2023-11-24 17:38:24 +0800

    增加ShimmerList组件

--- lib/widget/common/common_shimmer.dart
+++ lib/widget/common/common_shimmer.dart
@@ -74,3 +74,42 @@ Widget buildShimmerList() {
     },
   );
 }
+
+class ShimmerList extends StatelessWidget {
+  const ShimmerList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: 10, // 重复渲染10次，可以根据需要调整
+      itemBuilder: (context, index) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
+              child: Container(
+                width: 200.0,
+                height: 20.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
+              child: Container(
+                width: 300.0,
+                height: 16.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 32.0), // 调整需要的间距
+          ],
+        );
+      },
+    );
+  }
+}
