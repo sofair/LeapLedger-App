1fa42cf88d80ac9dafd6ce2fc6cfc422171d7fbf
小滋润 <807046079@qq.com>
2024-01-08 17:46:32 +0800

    LeftSlideRoute增加参数transitionDuration

--- lib/routes/animation.dart
+++ lib/routes/animation.dart
@@ -2,18 +2,17 @@ part of 'routes.dart';
 
 class LeftSlideRoute extends PageRouteBuilder {
   final Widget page;
-
-  LeftSlideRoute({required this.page})
-      : super(
+  LeftSlideRoute({
+    required this.page,
+    super.transitionDuration = const Duration(milliseconds: 800),
+  }) : super(
           pageBuilder: (context, animation, secondaryAnimation) => page,
-          transitionDuration: const Duration(milliseconds: 800),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             var begin = const Offset(-1.0, 0.0);
             var end = Offset.zero;
             var curve = Curves.ease;
 
-            var tween =
-                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
             return SlideTransition(
               position: animation.drive(tween),
