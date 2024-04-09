0e337a5b9d9755eaabced806c24fb714d7d73110
小滋润 <807046079@qq.com>
2024-04-09 22:11:14 +0800

    ”交易类型设置页“增加”交易类型关联页“跳转入口

--- lib/view/transaction/category/tree/transaction_category_tree.dart
+++ lib/view/transaction/category/tree/transaction_category_tree.dart
@@ -15,7 +15,8 @@ part 'widget/drag_and_drop_lists.dart';
 
 class TransactionCategoryTree extends StatefulWidget {
   final AccountDetailModel account;
-  const TransactionCategoryTree({super.key, required this.account});
+  final AccountDetailModel? relatedAccount;
+  const TransactionCategoryTree({super.key, required this.account, required this.relatedAccount});
 
   @override
   State<TransactionCategoryTree> createState() => _TransactionCategoryTreeState();
@@ -52,6 +53,19 @@ class _TransactionCategoryTreeState extends State<TransactionCategoryTree> with 
             offstage: false == widget.account.isCreator,
             child: IconButton(icon: const Icon(Icons.add_circle_outline), onPressed: () => onAddAction()),
           ),
+          Offstage(
+            offstage: widget.relatedAccount == null ||
+                !TransactionCategoryRouterGuard.accountMapping(
+                    parentAccount: widget.account, childAccount: widget.relatedAccount!),
+            child: TextButton(
+              child: const Text("关联交易"),
+              onPressed: () => TransactionCategoryRoutes.accountMapping(
+                context,
+                parentAccount: widget.account,
+                childAccount: widget.relatedAccount!,
+              ).push(),
+            ),
+          )
         ],
       ),
       body: PageStorage(
